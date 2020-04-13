b2f94560b2d672b3ad274cf5a8bab325d7ffd143
Svyat <dram1008@yandex.ru>
2020-04-13 20:59:38 +0300

    0.0.7

--- src/Model1.php
+++ src/Model1.php
@@ -2,12 +2,12 @@
 
 namespace iAvatar777\assets\JqueryUpload1;
 
-use avatar\models\Log;
 use common\models\school\File;
 use cs\Application;
 use cs\services\Str;
 use cs\services\Url;
 use cs\services\VarDumper;
+use Imagine\Image\Box;
 use Yii;
 use yii\base\Model;
 use cs\Widget\FileUpload2\FileUpload;
@@ -15,15 +15,27 @@ use yii\data\ActiveDataProvider;
 use yii\data\Sort;
 use yii\db\ActiveQuery;
 use yii\db\Query;
+use yii\helpers\ArrayHelper;
 use yii\helpers\FileHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\imagine\Image;
 
 /**
  *
  */
 class Model1 extends Model
 {
+    /** с обрезкой, по умолчанию */
+    const MODE_THUMBNAIL_CUT = 'outbound';
+
+    /** вписать */
+    const MODE_THUMBNAIL_FIELDS = 'inset';
+
+    /** вписать с фоном */
+    const MODE_THUMBNAIL_WHITE = 'white';
+
+
     /** @var  string */
     public $signature;
 
@@ -117,6 +129,152 @@ class Model1 extends Model
             'size'    => $size,
         ];
 
+        if ($this->updateJson) {
+            $updateRet = [];
+            foreach ($this->updateJson as $item) {
+                $function = $item['function'];
+                $index = $item['index'];
+                $options = $item['options'];
+                $file = \iAvatar777\assets\JqueryUpload1\File::path($path . $Upload->newFileName);
+                $destination = new \iAvatar777\assets\JqueryUpload1\SitePath('/upload/cloud/' . $folderName . '/'. $fileNameWithoutExt . '_' . $index . '.' . $ext);
+                if ($function == 'crop') {
+                    $mode = 'outbound';
+                    if ($options['mode'] == 'MODE_THUMBNAIL_CUT') $mode = 'outbound';
+                    if ($options['mode'] == 'MODE_THUMBNAIL_FIELDS') $mode = 'inset';
+                    if ($options['mode'] == 'MODE_THUMBNAIL_WHITE') $mode = 'white';
+                    self::saveImage(
+                        $file,
+                        $destination,
+                        [
+                            $options['width'],
+                            $options['height'],
+                            $mode,
+                            'quality' => \yii\helpers\ArrayHelper::getValue($options, 'quality', 100),
+                        ]
+                    );
+
+                    // Добавляю в БД
+                    $path = Yii::getAlias('@webroot' . $destination->getPath());
+                    if (file_exists($path)) {
+                        $size = filesize($path);
+                    } else {
+                        $size = 0;
+                    }
+
+                    $updateRet[$index] = [
+                        'url'  => \yii\helpers\Url::to($destination->getPath(), true),
+                        'size' => $size,
+                    ];
+                }
+            }
+            $ret['update'] = $updateRet;
+        }
+
+
+
         return $ret;
     }
+
+
+    /**
+     * Сохраняет картинку по формату
+     *
+     * @param \iAvatar777\assets\JqueryUpload1\File $file
+     * @param \iAvatar777\assets\JqueryUpload1\SitePath $destination
+     * @param array $field
+     * @param array | false $format => [
+     *                              3000,
+     *                              3000,
+     *                              FileUpload::MODE_THUMBNAIL_OUTBOUND
+     *                              'isExpandSmall' => true,
+     *                              ] ,
+     *
+     * @return \iAvatar777\assets\JqueryUpload1\SitePath
+     */
+    private function saveImage($file, $destination, $format)
+    {
+        if ($format === false || is_null($format)) {
+            $file->save($destination->getPathFull());
+            return $destination;
+        }
+
+        $widthFormat = 1;
+        $heightFormat = 1;
+        if (is_numeric($format)) {
+            // Обрезать квадрат
+            $widthFormat = $format;
+            $heightFormat = $format;
+        } else if (is_array($format)) {
+            $widthFormat = $format[0];
+            $heightFormat = $format[1];
+        }
+
+        // generate a thumbnail image
+        $mode = ArrayHelper::getValue($format, 2, self::MODE_THUMBNAIL_CUT);
+        if ($file->isContent()) {
+            $image = Image::getImagine()->load($file->content);
+        } else {
+            $image = Image::getImagine()->open($file->path);
+        }
+        if (ArrayHelper::getValue($format, 'isExpandSmall', true)) {
+            $image = self::expandImage($image, $widthFormat, $heightFormat, $mode);
+        }
+        $quality = ArrayHelper::getValue($format, 'quality', 80);
+        $options = ['quality' => $quality];
+        $image->thumbnail(new Box($widthFormat, $heightFormat), $mode)->save($destination->getPathFull(), $options);
+
+        return $destination;
+    }
+
+    /**
+     * Расширяет маленькую картинку по заданной стратегии
+     *
+     * @param \Imagine\Image\ImageInterface $image
+     * @param int $widthFormat
+     * @param int $heightFormat
+     * @param int $mode
+     *
+     * @return \Imagine\Image\ImageInterface
+     */
+    protected static function expandImage($image, $widthFormat, $heightFormat, $mode)
+    {
+        $size = $image->getSize();
+        $width = $size->getWidth();
+        $height = $size->getHeight();
+        if ($width < $widthFormat || $height < $heightFormat) {
+            // расширяю картинку
+            if ($mode == self::MODE_THUMBNAIL_CUT) {
+                if ($width < $widthFormat && $height >= $heightFormat) {
+                    $size = $size->widen($widthFormat);
+                } else if ($width >= $widthFormat && $height < $heightFormat) {
+                    $size = $size->heighten($heightFormat);
+                } else if ($width < $widthFormat && $height < $heightFormat) {
+                    // определяю как расширять по ширине или по высоте
+                    if ($width / $widthFormat < $height / $heightFormat) {
+                        $size = $size->widen($widthFormat);
+                    } else {
+                        $size = $size->heighten($heightFormat);
+                    }
+                }
+                $image->resize($size);
+            } else {
+                if ($width < $widthFormat && $height >= $heightFormat) {
+                    $size = $size->heighten($heightFormat);
+                } else if ($width >= $widthFormat && $height < $heightFormat) {
+                    $size = $size->widen($widthFormat);
+                } else if ($width < $widthFormat && $height < $heightFormat) {
+                    // определяю как расширять по ширине или по высоте
+                    if ($width / $widthFormat < $height / $heightFormat) {
+                        $size = $size->heighten($heightFormat);
+                    } else {
+                        $size = $size->widen($widthFormat);
+                    }
+                }
+                $image->resize($size);
+            }
+        }
+
+        return $image;
+    }
+
 }
---
+++ src/Str.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace iAvatar777\assets\JqueryUpload1;
+
+use yii\helpers\ArrayHelper;
+
+class Str
+{
+    public static $encoding = 'utf-8';
+
+    public static function replace($find, $replace, $content, $option = null)
+    {
+        return mb_ereg_replace($find, $replace, $content, $option);
+    }
+
+    public static function length($string)
+    {
+        return mb_strlen($string, self::$encoding);
+    }
+
+    public static function sub($string, $start, $length = null)
+    {
+        return mb_substr($string, $start, $length, self::$encoding);
+    }
+
+    public static function pos($find, $content, $offset = null)
+    {
+        return mb_strpos($content, $find, $offset, self::$encoding);
+    }
+
+    /**
+     * Превращает строку в camelCase style
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public static function camelCase($string)
+    {
+        return strtoupper(substr($string, 0, 1)) . substr($string, 1);
+    }
+
+
+    public static function getChars($string)
+    {
+        $len = self::length($string);
+        $ret = [];
+        for ($i = 0; $i < $len; $i++) {
+            $ret[ $i ] = self::sub($string, $i, 1);
+        }
+
+        return $ret;
+    }
+
+    public static function rus2translit($string)
+    {
+        $maxLength = 30;
+        $converter = [
+            'а' => 'a', 'б' => 'b', 'в' => 'v',
+            'г' => 'g', 'д' => 'd', 'е' => 'e',
+            'ё' => 'e', 'ж' => 'zh', 'з' => 'z',
+            'и' => 'i', 'й' => 'y', 'к' => 'k',
+            'л' => 'l', 'м' => 'm', 'н' => 'n',
+            'о' => 'o', 'п' => 'p', 'р' => 'r',
+            'с' => 's', 'т' => 't', 'у' => 'u',
+            'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+            'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch',
+            'ь' => '', 'ы' => 'y', 'ъ' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+
+            'A' => 'a', 'B' => 'b', 'C' => 'c',
+            'D' => 'd', 'E' => 'e', 'F' => 'f',
+            'G' => 'g', 'H' => 'h', 'I' => 'i',
+            'J' => 'j', 'K' => 'k', 'L' => 'l',
+            'M' => 'm', 'N' => 'n', 'O' => 'o',
+            'P' => 'p', 'Q' => 'q', 'R' => 'r',
+            'S' => 's', 'T' => 't', 'U' => 'u',
+            'V' => 'v', 'W' => 'w', 'X' => 'x',
+            'Y' => 'y', 'Z' => 'z',
+
+            'a' => 'a', 'b' => 'b', 'c' => 'c',
+            'd' => 'd', 'e' => 'e', 'f' => 'f',
+            'g' => 'g', 'h' => 'h', 'i' => 'i',
+            'j' => 'j', 'k' => 'k', 'l' => 'l',
+            'm' => 'm', 'n' => 'n', 'o' => 'o',
+            'p' => 'p', 'q' => 'q', 'r' => 'r',
+            's' => 's', 't' => 't', 'u' => 'u',
+            'v' => 'v', 'w' => 'w', 'x' => 'x',
+            'y' => 'y', 'z' => 'z',
+
+            'А' => 'a', 'Б' => 'b', 'В' => 'v',
+            'Г' => 'g', 'Д' => 'd', 'Е' => 'e',
+            'Ё' => 'e', 'Ж' => 'zh', 'З' => 'z',
+            'И' => 'i', 'Й' => 'y', 'К' => 'k',
+            'Л' => 'l', 'М' => 'm', 'Н' => 'n',
+            'О' => 'o', 'П' => 'p', 'Р' => 'r',
+            'С' => 's', 'Т' => 't', 'У' => 'u',
+            'Ф' => 'f', 'Х' => 'h', 'Ц' => 'c',
+            'Ч' => 'ch', 'Ш' => 'sh', 'Щ' => 'sch',
+            'Ь' => '', 'Ы' => 'y', 'Ъ' => '',
+            'Э' => 'e', 'Ю' => 'yu', 'Я' => 'ya',
+            ' ' => '_',
+
+            '1' => '1',
+            '2' => '2',
+            '3' => '3',
+            '4' => '4',
+            '5' => '5',
+            '6' => '6',
+            '7' => '7',
+            '8' => '8',
+            '9' => '9',
+            '0' => '0',
+        ];
+        $ret = '';
+        foreach (self::getChars($string) as $char) {
+            $ret .= ArrayHelper::getValue($converter, $char, '');
+        }
+        $ret = substr($ret,0,$maxLength);
+
+        return $ret;
+    }
+
+    public static function toLower($string)
+    {
+        $converter = [
+            'А' => 'а', 'Б' => 'б', 'В' => 'в',
+            'Г' => 'г', 'Д' => 'д', 'Е' => 'е',
+            'Ё' => 'ё', 'Ж' => 'ж', 'З' => 'з',
+            'И' => 'и', 'Й' => 'й', 'К' => 'к',
+            'Л' => 'л', 'М' => 'м', 'Н' => 'н',
+            'О' => 'о', 'П' => 'п', 'Р' => 'р',
+            'С' => 'с', 'Т' => 'т', 'У' => 'у',
+            'Ф' => 'ф', 'Х' => 'х', 'Ц' => 'ц',
+            'Ч' => 'ч', 'Ш' => 'ш', 'Щ' => 'щ',
+            'Ь' => 'ь', 'Ы' => 'ы', 'Ъ' => 'ъ',
+            'Э' => 'э', 'Ю' => 'ю', 'Я' => 'я',
+        ];
+        $ret = '';
+        foreach (self::getChars($string) as $char) {
+            $ret .= ArrayHelper::getValue($converter, $char, $char);
+        }
+
+        return $ret;
+    }
+
+    public static function toUpper($string)
+    {
+        $converter = [
+            'а' => 'А', 'б' => 'Б', 'в' => 'В',
+            'г' => 'Г', 'д' => 'Д', 'е' => 'Е',
+            'ё' => 'Ё', 'ж' => 'Ж', 'з' => 'З',
+            'и' => 'И', 'й' => 'Й', 'к' => 'К',
+            'л' => 'Л', 'м' => 'М', 'н' => 'Н',
+            'о' => 'О', 'п' => 'П', 'р' => 'Р',
+            'с' => 'С', 'т' => 'Т', 'у' => 'У',
+            'ф' => 'Ф', 'х' => 'Х', 'ц' => 'Ц',
+            'ч' => 'Ч', 'ш' => 'Ш', 'щ' => 'Щ',
+            'ь' => 'Ь', 'ы' => 'Ы', 'ъ' => 'Ъ',
+            'э' => 'Э', 'ю' => 'Ю', 'я' => 'Я',
+        ];
+        $ret = '';
+        foreach (self::getChars($string) as $char) {
+            $ret .= ArrayHelper::getValue($converter, $char, $char);
+        }
+
+        return $ret;
+    }
+
+    public static function isUpper($char)
+    {
+        $converter = [
+            'а' => 'А', 'б' => 'Б', 'в' => 'В',
+            'г' => 'Г', 'д' => 'Д', 'е' => 'Е',
+            'ё' => 'Ё', 'ж' => 'Ж', 'з' => 'З',
+            'и' => 'И', 'й' => 'Й', 'к' => 'К',
+            'л' => 'Л', 'м' => 'М', 'н' => 'Н',
+            'о' => 'О', 'п' => 'П', 'р' => 'Р',
+            'с' => 'С', 'т' => 'Т', 'у' => 'У',
+            'ф' => 'Ф', 'х' => 'Х', 'ц' => 'Ц',
+            'ч' => 'Ч', 'ш' => 'Ш', 'щ' => 'Щ',
+            'ь' => 'Ь', 'ы' => 'Ы', 'ъ' => 'Ъ',
+            'э' => 'Э', 'ю' => 'Ю', 'я' => 'Я',
+        ];
+
+        return in_array($char, $converter);
+    }
+
+    /**
+     * Возвращает ответ на вопрос
+     * Есть ли строка $find в $source?
+     *
+     * @param $source
+     * @param $find
+     *
+     * @return int
+     */
+    public static function isContain($source, $find)
+    {
+        return mb_strpos($source, $find) !== false;
+    }
+}
